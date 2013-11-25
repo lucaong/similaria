5a6ae606c1b54ce5a45bd23b3438384a064cc7bc
Luca Ongaro <lukeongaro@gmail.com>
2013-11-25 01:01:50 +0100

    add count to Neighbor and add documentation

--- src/main/scala/com/lucaongaro/similaria/Neighbor.scala
+++ src/main/scala/com/lucaongaro/similaria/Neighbor.scala
@@ -1,9 +1,16 @@
 package com.lucaongaro.similaria
 
-case class Neighbor( item: Int, similarity: Double )
+/** The representation of an item as a neighbor of another reference item
+  *
+  * @constructor creates a new neighbor
+  * @param item the neighbor item
+  * @param similarity the similarity to the reference item
+  * @param count the occurrency count of the neighbor item
+  */
+case class Neighbor( item: Int, similarity: Double, count: Int )
 
 object Neighbor {
-  // Default ordering is by descending similarity
+  /** Default ordering of neighbors by descending similarity */
   implicit val defaultOrdering = Ordering.fromLessThan[Neighbor] { ( a, b ) =>
     if ( a.similarity == b.similarity )
       a.item > b.item
