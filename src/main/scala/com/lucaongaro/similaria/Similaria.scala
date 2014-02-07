80fe407dc96c1efd52b2ba9f9c1cda23c6d62b4d
Luca Ongaro <lukeongaro@gmail.com>
2014-02-07 16:05:31 +0100

    do not duplicate documentation of methods
    keeping it on the trait is enough

--- src/main/scala/com/lucaongaro/similaria/Similaria.scala
+++ src/main/scala/com/lucaongaro/similaria/Similaria.scala
@@ -22,12 +22,6 @@ class Similaria(
 
   val dbm = new DBManager( opts.dbPath, opts.dbSize )
 
-  /** Adds a preference set
-    *
-    * @param prefSet the preference set to be added
-    * @param count how many times the preference set should be added
-    * @return the set that was added
-    */
   def addPreferenceSet(
     prefSet: PreferenceSet,
     count:   Int = 1
@@ -36,13 +30,6 @@ class Similaria(
     prefSet
   }
 
-  /** Appends a subset to an already existing preference set
-    *
-    * @param originalSet the pre-existing set
-    * @param setToAdd the subset to be added
-    * @param count how many times the subset should be added
-    * @return the resulting set
-    */
   def addToPreferenceSet(
     originalSet: PreferenceSet,
     setToAdd:    PreferenceSet,
@@ -53,12 +40,6 @@ class Similaria(
     originalSet | set
   }
 
-  /** Removes a preference set
-    *
-    * @param prefSet the preference set to be removed
-    * @return the set that was removed
-    * @param count how many times the preference set should be removed
-    */
   def removePreferenceSet(
     prefSet: PreferenceSet,
     count:   Int = 1
@@ -67,13 +48,6 @@ class Similaria(
     prefSet
   }
 
-  /** Removes a subset from an existing preference set
-    *
-    * @param originalSet the pre-existing set
-    * @param setToRemove the subset to be removed
-    * @param count how many times the subset should be removed
-    * @return the resulting set
-    */
   def removeFromPreferenceSet(
     originalSet: PreferenceSet,
     setToRemove: PreferenceSet,
@@ -84,12 +58,6 @@ class Similaria(
     originalSet &~ set
   }
 
-  /** Returns the nearest neighbors of the given item
-    *
-    * @param item the reference item
-    * @param limit the maximum number of neighbors to retrieve
-    * @return a sorted set of the nearest neighbors
-    */
   def findNeighborsOf(
     item:  Int,
     limit: Int = 10
@@ -109,12 +77,6 @@ class Similaria(
     }
   }
 
-  /** Returns the similarity between two items
-    *
-    * @param item the first item
-    * @param other the other item
-    * @return the similarity between item and other
-    */
   def getSimilarityBetween(
     item:  Int,
     other: Int
@@ -129,20 +91,12 @@ class Similaria(
     similarity( itemCount, otherCount, coCount )
   }
 
-  /** Mutes an item (so that it is not considered when getting neighbors)
-    *
-    * @param item the item to be muted
-    */
   def muteItem(
     item: Int
   ) {
     dbm.setMuted( item, true )
   }
 
-  /** Unmutes an item (so that it is considered when getting neighbors)
-    *
-    * @param item the item to be unmuted
-    */
   def unmuteItem(
     item: Int
   ) {
